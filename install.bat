dee7d86b68fd40f036f644dc94f27a543b5129a6
Danilo Horta <danilo.horta@pm.me>
2021-02-23 11:42:07 +0000

    temporary folder

--- install.bat
+++ install.bat
@@ -1,27 +1,27 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: Log file
 set ORIGIN=%cd%
-set LOGFILE=%ORIGIN%\zlib_install.log
 
 :: Configuration
 set VERSION=1.2.11
-set FILE=zlib-%VERSION%.zip
-set DIR=zlib-%VERSION%
-set URL=https://zlib.net/zlib%VERSION:.=%.zip
+set VER=%VERSION:.=%
+set URL=https://zlib.net/zlib%VER%.zip
 set CMAKE_VS_PLATFORM_NAME=x64
-set SRC_DIR=%ORIGIN%/%DIR%
-set BUILD_DIR=%SRC_DIR%/build
-
-echo %SRC_DIR%
-echo %BUILD_DIR%
+set TMPDIR=%HOMEDRIVE%\%HOMEPATH%\zlib_tmp
+set SRC_DIR=%TMPDIR%\zlib-%VERSION%
+set ZIPFILE=zlib%VER%.zip
+set ABS_ZIPFILE=%TMPDIR%\%ZIPFILE%
+set BUILD_DIR=%SRC_DIR%\build
+set LOGFILE=%TMPDIR%\zlib_install.log
 
 echo [0/6] Library(zlib==%VERSION%)
 
-call :cleanup
+if not exist %TMPDIR% (mkdir %TMPDIR% && cd %TMPDIR% || exit /B 1)
+call :cleanup_src
 call :cleanup_log
-call :log_sysinfo
+copy /y nul %LOGFILE% >nul 2>&1
+call :log_sysinfo >>%LOGFILE% 2>&1
 
 echo|set /p="[1/6] Checking cmake... "
 call :setup_cmake_path >>%LOGFILE% 2>&1
@@ -34,14 +34,12 @@ if not defined CMAKE (
 
 echo|set /p="[2/6] Downloading... "
 echo Fetching %URL% >>%LOGFILE% 2>&1
-call :winget "%URL%" >>%LOGFILE% 2>&1
+cd %TMPDIR% && call :winget "%URL%" >>%LOGFILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (call :failed && exit /B 1) else (echo done.)
 
 echo|set /p="[3/6] Extracting... "
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%FILE%', '.'); }" >>%LOGFILE% 2>&1
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%ABS_ZIPFILE%', '%TMPDIR%'); }" >>%LOGFILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (call :failed && exit /B 1) else (echo done.)
-
-cd %SRC_DIR%
 
 echo|set /p="[4/6] Fixing CMakeLists.txt... "
 set OLDSTR=RUNTIME DESTINATION ""\${INSTALL_BIN_DIR}\""
@@ -64,8 +62,8 @@ set NEWSTR=DESTINATION ""include\""
 call :search_replace "%OLDSTR%" "%NEWSTR%"
 if %ERRORLEVEL% NEQ 0 (call :failed && exit /B 1) else (echo done.)
 
-rd /S /Q build >nul 2>&1
-mkdir build && cd build
+rd /S /Q %BUILD_DIR% >nul 2>&1
+mkdir %BUILD_DIR% && cd %BUILD_DIR%
 
 echo|set /p="[5/6] Configuring... "
 "%CMAKE%" .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%PROGRAMFILES%\zlib" >>%LOGFILE% 2>&1
@@ -75,7 +73,8 @@ echo [6/6] Compiling and installing...
 "%CMAKE%" --build . --config Release --target install
 if %ERRORLEVEL% NEQ 0 (call :failed && exit /B 1) else (echo done.)
 
-call :cleanup
+call :cleanup_src
+call :goto_origin
 
 echo Details can be found at %LOGFILE%.
 
@@ -102,26 +101,23 @@ if exist !DIR_PATH!\cmake.exe (
 )
 @goto :eof
 
-:cleanup
-cd %ORIGIN% >nul 2>&1
-del /Q %FILE% ! >nul 2>&1
-rd /S /Q %DIR% >nul 2>&1
+:cleanup_src
+del /Q %ABS_ZIPFILE% ! >nul 2>&1
+rd /S /Q %SRC_DIR% >nul 2>&1
 @goto :eof
 
 :cleanup_log
-cd %ORIGIN% >nul 2>&1
 del /Q %LOGFILE% ! >nul 2>&1
-copy /y nul %LOGFILE% >nul 2>&1
 @goto :eof
 
 :log_sysinfo
-echo -- Environment variables >>%LOGFILE% 2>&1
-set >>%LOGFILE% 2>&1
-echo[ >>%LOGFILE% 2>&1
+echo -- Environment variables
+set
+echo[
 
-echo -- System info >>%LOGFILE% 2>&1
-systeminfo | findstr /B /C:"OS Name" /C:"OS Version" >>%LOGFILE% 2>&1
-echo[ >>%LOGFILE% 2>&1
+echo -- System info
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version"
+echo[
 @goto :eof
 
 :cmakefiles_debug
@@ -141,24 +137,24 @@ if exist %FILE% (
 
 :failed
 echo FAILED.
-call :cmakefiles_debug
+call :cmakefiles_debug >>%LOGFILE% 2>&1
 echo[
 echo ---------------------------------------- log begin ----------------------------------------
 type %LOGFILE%
 echo ----------------------------------------  log end  ----------------------------------------
 echo LOG: %LOGFILE%
-call :cleanup
+call :cleanup_src
+call :goto_origin
 @goto :eof
 
-:joinpath
-set Path1=%~1
-set Path2=%~2
-if {%Path1:~-1,1%}=={\} (set Result=%Path1%%Path2%) else (set Result=%Path1%\%Path2%)
-goto :eof
+:goto_origin
+cd %ORIGIN% >nul 2>&1
+@goto :eof
 
 :search_replace
 set OLDSTR=%~1
 set NEWSTR=%~2
+cd %SRC_DIR%
 set CMD="(gc CMakeLists.txt) -replace '%OLDSTR%', '%NEWSTR%' | Out-File -encoding ASCII CMakeLists.txt"
 powershell -Command %CMD%  >>%LOGFILE% 2>&1
 goto :eof
